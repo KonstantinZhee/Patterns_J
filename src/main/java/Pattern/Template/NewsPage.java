6bc73fea7026a4513bc21e8c1a6490fc9176b54d
”Konstantin_Zhe” <konstantin.zheltukhin@devim.team>
2024-01-10 20:54:57 +0300

    add Template Pattern

---
+++ src/main/java/Pattern/Template/NewsPage.java
@@ -0,0 +1,9 @@
+package Pattern.Template;
+
+public class NewsPage {
+    public void showPage(){
+        System.out.println("Header");
+        System.out.println("NEWS");
+        System.out.println("Footer");
+    }
+}
---
+++ src/main/java/Pattern/Template/WebsiteRunner.java
@@ -0,0 +1,7 @@
+package Pattern.Template;
+
+public class WebsiteRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
