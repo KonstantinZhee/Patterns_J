79499490f20b17fb4767983b9fed28c9285c10e2
”Konstantin_Zhe” <konstantin.zheltukhin@devim.team>
2024-01-16 18:37:25 +0300

    Add Facade

---
+++ src/main/java/Patterns/Facade/SubSystem/Proz.java
@@ -0,0 +1,13 @@
+package Patterns.Facade.SubSystem;
+
+public class Proz {
+
+  public void powerOn() {
+    System.out.println("Proz is on");
+  }
+
+  public void powerOff() {
+    System.out.println("Prozz is off");
+  }
+
+}
---
+++ src/main/java/Patterns/Facade/SubSystem/Video_Card.java
@@ -0,0 +1,17 @@
+package Patterns.Facade.SubSystem;
+
+public class Video_Card {
+
+  public void powerOn() {
+    System.out.println("Video card is on");
+  }
+
+  public void powerOff() {
+    System.out.println("Video card is off");
+  }
+
+  public void showPicture() {
+    System.out.println("Video card shows picture");
+  }
+
+}
