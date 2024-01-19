427bca5ca4998409c39596b4289aa5b2a6a107fe
”Konstantin_Zhe” <konstantin.zheltukhin@devim.team>
2024-01-19 18:48:44 +0300

    add Prototype

---
+++ src/main/java/Patterns/Creational/Prototype/Example/Serialization/Ex.java
@@ -0,0 +1,26 @@
+package Patterns.Creational.Prototype.Example.Serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Ex {
+
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
+    User u1 = new User("u1", 23);
+
+    //Create a thread chain with an object output stream at the end
+    ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream("test1.dat"));
+    ous.writeObject(u1);
+    System.out.println(u1);
+    ous.close();
+
+    //Objects are read in the same order in which they were written to the file
+    ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test1.dat"));
+    User clone = (User) ois.readObject();
+    System.out.println(clone);
+    ois.close();
+  }
+}
---
+++ src/main/java/Patterns/Creational/Prototype/Example/WithJDK/Ex.java
@@ -0,0 +1,26 @@
+package Patterns.Creational.Prototype.Example.WithJDK;
+
+import java.util.GregorianCalendar;
+
+public class Ex {
+
+  public static void main(String[] args) {
+    User u1 = new User("u1", 23);
+    User u2 = null;
+    u1.setCalendar(new GregorianCalendar(1999, 2, 2));
+    System.out.println(u1);
+
+    try {
+      u2 = (User) u1.clone();
+      //Different objects bc Calendar impl Cloneable
+      u2.setAge(222);
+      u2.setCalendar(new GregorianCalendar(1, 1, 1));
+    } catch (CloneNotSupportedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("U1 ==  " + u1);
+
+    System.out.println("U2 ==  " + u2.toString());
+  }
+}
