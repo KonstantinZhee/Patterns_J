89bb30d565ae56fc75cbbd33f501d07a6ba94404
”Konstantin_Zhe” <konstantin.zheltukhin@devim.team>
2024-01-21 23:52:57 +0300

    add Memento

---
+++ src/main/java/Patterns/Behavioral/Memento/Example/StaticNestedMemento/Caretaker.java
@@ -0,0 +1,20 @@
+package Patterns.Behavioral.Memento.Example.StaticNestedMemento;
+
+import java.util.Stack;
+
+public class Caretaker {
+
+  private final Stack<Creator.Memento> mementos = new Stack<>();
+
+  public Creator.Memento getLastSave() {
+    return mementos.peek();
+  }
+
+  public void addSave(Creator.Memento memento) {
+    mementos.push(memento);
+  }
+
+  public void removeLastSave() {
+    mementos.pop();
+  }
+}
---
+++ src/main/java/Patterns/Behavioral/Memento/Example/StaticNestedMemento/Ex.java
@@ -0,0 +1,37 @@
+package Patterns.Behavioral.Memento.Example.StaticNestedMemento;
+
+// We can also create Memento in package class
+// Encapsulation is still worked,
+// bc new Memento is created by Creator(Class can use private fields)
+// Also can create common interface for Memento
+
+//Memento can have link to Creator and restore him, by itself
+public class Ex {
+
+  public static void main(String[] args) {
+    Creator ork = new Creator("Ork", "Bowman", "dang");
+    Creator undead = new Creator("Undead", "Wizard", "world");
+
+    Caretaker caretaker = new Caretaker();
+    caretaker.addSave(ork.save()); //First save
+
+    System.out.println(ork.toString());
+    ork.upLvl();
+    ork.upLvl();
+    ork.setState("world");
+    ork.setType("Super Bowman");
+    System.out.println(ork.toString());
+    caretaker.addSave(ork.save()); // Next save
+
+    ork.upLvl();
+    ork.setState("Super dang");
+    ork.upLvl();
+    ork.upLvl();
+    System.out.println(ork.toString());
+    System.out.println("SORRY YOU DIED LOL");
+    ork.load(caretaker.getLastSave());
+    System.out.println(ork.toString());
+
+    undead.load(caretaker.getLastSave());
+  }
+}
